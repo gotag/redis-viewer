4c8b77804e4d8166df733ec9839dee4b77c403fd
Ollie Marshall <ollie.marshall@btinternet.com>
2011-07-24 13:41:43 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'haml'
+require 'sinatra'
+require 'redis'
+
+helpers do
+  def redis
+    @redis ||= Redis.new
+  end
+end
+
+get "/" do
+  @keys = redis.keys("*")
+  haml :index
+end
+
+get "/:key" do
+  @key = params[:key]
+  @data = case redis.type(@key)
+  when "string"
+    Array(redis[@key])
+  when "list"
+    redis.lrange(@key, 0, -1)
+  when "set"
+    redis.smembers(@key)
+  else
+    []
+  end
+  haml :show
+end
